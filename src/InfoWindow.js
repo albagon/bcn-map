62cf82ec28d1f533198637f82c99554a892fde9b
albagonzalez <albupas@hotmail.com>
2019-01-16 13:34:16 +0000

    feat: Add venue details to infowindow

--- src/InfoWindow.js
+++ src/InfoWindow.js
@@ -5,7 +5,31 @@ const InfoWindow = ({ venue }) => {
   /*return '<div id="content">'+
       '<p>'+venues[index].name+'</p>'+
       '</div>'*/
-  return <div><h1>Hello {venue.name}</h1></div>
+  return <div className='contentHolder'>
+          <div className='iconCol'>
+            <div className='iconContainer'>
+              <img className='icon' src={venue.categories[0].icon.prefix+'64'+venue.categories[0].icon.suffix} alt={venue.categories[0].name} />
+            </div>
+          </div>
+          <div className='infoCol'>
+            <div className='venueBlock'>
+              <div className='venueDetails'>
+                <div className='venueName'>
+                  <h2><a href='/v/starbucks/5b65e0cfad910e002c4d4d6e' target='_blank'>{venue.name}</a></h2>
+                </div>
+                <div className='venueAddressData'>
+                  <div className='venueData'>
+                    <span className='categoryName'>{venue.categories[0].name}</span>
+                  </div>
+                  <div className='venueAddress'>{venue.location.formattedAddress.join(', ')}</div>
+                </div>
+              </div>
+            </div>
+            <div className='resultFooter'>
+              <a href='/v/starbucks/5b65e0cfad910e002c4d4d6e' target='_blank'>More details</a>
+            </div>
+          </div>
+        </div>
 }
 
 export default InfoWindow
